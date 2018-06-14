df707c8f7e54ebcd705e457bf22d7b5b387f6d97
Priscilla H <priscillamhuynh@gmail.com>
2018-06-14 09:08:02 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -2,7 +2,7 @@ class TicTacToe
   def initialize(board = nil)
     @board = board || Array.new(9," ")
   end
-  
+
   WIN_COMBINATIONS = [
     [0,1,2], #Top row
     [3,4,5], #Middle row
@@ -14,13 +14,14 @@ class TicTacToe
     [2,4,6]  #Right diagonal
   ]
 
-  def display_board
+  def display_board(board)
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
   end
-  display_board
   
+  display_board
+
 end
