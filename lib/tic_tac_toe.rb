41d653e1a58bc6de2425e938ea61b3297aa90274
Priscilla H <priscillamhuynh@gmail.com>
2018-06-14 09:11:24 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -21,4 +21,14 @@ class TicTacToe
     puts "-----------"
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
+
+  def input_to_index(user_input)
+      user_input.to_i - 1
+    end
+
+    def move(position, char)
+      @board[position] = char
+    end
+
 end
+
